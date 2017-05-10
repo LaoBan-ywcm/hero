852db6d139f0a0bb14835b191caba12c0433ce76
LaoBan-ywcm <2459007415@qq.com>
2017-05-10 21:47:51 +0800

    HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes/heroes.component';
@@ -10,6 +15,7 @@ import {RouterModule} from "@angular/router";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {HeroService} from "./hero.service";
 import {AppRoutingModule} from "./app-routing.module";
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 
@@ -18,13 +24,15 @@ import {AppRoutingModule} from "./app-routing.module";
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,//[(ngmodule)]
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes/heroes.component.ts
+++ src/app/heroes/heroes/heroes.component.ts
@@ -30,8 +30,30 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void{
     this.getHeroes();
   }
-
+  //选中英雄
   onSelect(hero: Hero):void{
     this.selectedHero = hero;
   }
+  //添加英雄
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+    //删除英雄
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+
+
 }
